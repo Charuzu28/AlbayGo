7385f0fbd9fcf57c79dc3c6b587528544dc903e1
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-12-21 11:08:43 +0800

    Added Landing page

---
+++ client/src/components/SearchInput.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function SearchInput() {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!query.trim()) return;
+
+    console.log("User query:", query);
+    setQuery("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="w-full">
+      <input
+        type="text"
+        placeholder="Ask how to get around Albay… (Press Enter)"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="
+          w-full
+          max-w-xs
+          sm:max-w-sm
+          md:max-w-xl
+          lg:max-w-4xl
+          px-4 py-3
+          border border-gray-300
+          rounded-xl
+          text-base
+          focus:outline-none
+          focus:ring-2 focus:ring-black
+          font-poppins
+        "
+      />
+    </form>
+  );
+}
