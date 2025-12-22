75677fb9e186965c86f99b228d38e0f05c4f5767
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-12-22 22:32:21 +0800

    Added Typing Indicator

---
+++ client/src/components/TypingIndicator.jsx
@@ -0,0 +1,8 @@
+
+export default function TypingIndicator(){
+    return (
+        <div className="flex justify-start px-4 text-gray-500">
+            AlbayGo is typing<span className="animate-pulse">...</span>
+        </div>
+    )
+}
